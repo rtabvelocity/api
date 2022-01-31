07089e537d4248299acc02a53388f0f868caddda
Ron Tablan <rtablan@VA-BUR.local>
2022-01-31 12:23:42 -0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let button = document.querySelector("button");
+
+
+button.addEventListener("click", function(){
+
+    alert("It was clicked");
+    console.log("Clicked");
+})
+
+let lotNums = async function(){
+
+
+};
