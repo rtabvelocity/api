888fee06dcab673111f5e7d506cecc0081585d0d
Ron Tablan <rtablan@VA-BUR.local>
2022-02-01 07:43:20 -0800

    updates

--- js/script.js
+++ js/script.js
@@ -3,11 +3,33 @@ let button = document.querySelector("button");
 
 button.addEventListener("click", function(){
 
-    alert("It was clicked");
-    console.log("Clicked");
+    // alert("It was clicked");
+    // console.log("Clicked");
+    lotNums();
+   
 })
+
+const username ="rtabvelocity";
 
 let lotNums = async function(){
 
+        
+        const userRequest = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1&api_key=DEMO_KEY`);
+        const marsData = await userRequest.json();
+        // imageMars=(marsData);
+        console.log(marsData);
 
+        const arraymarsData = [];
+    // for(let elements of marsData){
+    //     // elements.push(arraymarsData);
+    //     // console.log(marsData;
+        
+    // }
+        return;
+       
 };
+
+let imageMars = function(marsData){
+    
+
+   };
